dd23d0cf99a3cde095ca4659f8144b78a49dd684
Jordan Schulte <email.jordanschulte@gmail.com>
2026-04-30 19:21:57 -0400

    Changed Front-end Engineer to Web Developer

--- src/app/components/layout/bio/Bio.jsx
+++ src/app/components/layout/bio/Bio.jsx
@@ -22,7 +22,7 @@ const Bio = () => {
           {admin ? <span className='admin-mode'>Admin Mode</span> : ''}
             <span className='bio-info-name'><AnimatedText text='Jordan Schulte'/></span>
             <div className='bio-info-location'><AnimatedText text='Buffalo, NY'/></div>
-            <div className='bio-info-role'><AnimatedText text='Front-End Engineer'/></div>
+            <div className='bio-info-role'><AnimatedText text='Web Developer'/></div>
             <div className='bio-links-wrapper'>
                 <a href="https://github.com/JSCHLTE" target='_blank' rel='noreferrer noopener' aria-label='Checkout my GitHub' className='button-press'><i className="fa-brands fa-github" aria-hidden="true"></i></a>
                 <a href="https://www.linkedin.com/in/jordan-schulte-9a5961216/" target='_blank' rel='noreferrer noopener' aria-label='Checkout my LinkedIn' className='button-press'><i className="fa-brands fa-linkedin" aria-hidden="true"></i></a>
